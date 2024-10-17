4ceb7679fac2fbb320c5b4930b7086ca1d686870
Tarek-Bohdima <tarek.buhdeima@gmail.com>
2024-10-17 10:43:51 +0300

    fix hilt

--- app/src/main/java/com/example/android/dogsapp/common/di/activity/ActivityScope.kt
+++
@@ -1,6 +0,0 @@
-package com.example.android.dogsapp.common.di.activity
-
-import javax.inject.Scope
-
-@Scope
-annotation class ActivityScope
--- app/src/main/java/com/example/android/dogsapp/common/di/application/ApplicationScope.kt
+++
@@ -1,6 +0,0 @@
-package com.example.android.dogsapp.common.di.application
-
-import javax.inject.Scope
-
-@Scope
-annotation class ApplicationScope
